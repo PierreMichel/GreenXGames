e29b8d5d03c5cf553fcb9cb4d4cb87a65210e8eb
unknown <pierre.michel@e-artsup.net>
2018-06-16 04:08:34 +0200

    Modifications unity Shader Garph _PierreMichel + touch mobil

---
+++ GreenXGames/Assets/Scripts/Menu/SelectZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectZone : MonoBehaviour
+{
+    [Header("Paramètres")]
+    public GameObject UIBatiments;
+
+    private void OnMouseUp() // Si l'on appuis sur l'objet, la fonction s'execute.
+    {
+        UIBatiments.SetActive(true);
+    }
+}
---
+++ GreenXGames/Assets/Scripts/Menu/SpawnBatiment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBatiment : MonoBehaviour {
+
+    public GameObject Batiment;
+    public Transform[] SpawnPosition;
+    public GameObject PanelActuel;
+
+	public void Spawn()
+    {
+        GameObject bat = Instantiate(Batiment);
+        int SpawnNumber = Random.Range(0, SpawnPosition.Length);
+        bat.transform.parent = SpawnPosition[SpawnNumber] ;
+        bat.transform.position = SpawnPosition[SpawnNumber].position;
+        bat.transform.rotation = SpawnPosition[SpawnNumber].rotation;
+        PanelActuel.SetActive(false);
+
+    }
+}
